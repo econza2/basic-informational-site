f8277b14d80757b8682d2ad2eb9bac55c48cf970
econza2 <thisisforodin@proton.me>
2024-11-23 15:28:05 +0300

    Add basics to site

---
+++ index.js
@@ -0,0 +1,52 @@
+let http = require("http");
+let url = require("url");
+let fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+
+    if (filename === "./") {
+      fs.readFile("./index.html", function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (filename === "./contact-me") {
+      fs.readFile("./contact-me.html", function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (filename === "./about") {
+      fs.readFile("./about.html", function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("./404.html", function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080);
